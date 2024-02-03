9287d71f1469c74c761b1effc5997dad4099acd0
Sofia Ruiz <i201710029@cibertec.edu.pe>
2024-02-02 23:18:59 -0500

    Se actualiza estudiantes

--- src/main/java/pruebas/EstudiantePropuesta.java
+++ src/main/java/pruebas/EstudiantePropuesta.java
@@ -1,5 +1,21 @@
 package pruebas;
 
-public class EstudiantePropuesta {
+import DAOS.EstudianteDao;
+import DAOS.EstudianteDaoImpl;
+import model.Estudiante;
 
+public class EstudiantePropuesta {
+public static void main(String[] args) {
+		
+		EstudianteDao studentDao = new EstudianteDaoImpl();
+		
+		Estudiante estudiante = new Estudiante();
+		
+		//Crear
+		
+		estudiante.setNombresApellidos("Jose Perez");
+		estudiante.setTipoDocumento(3);
+		
+		
+	}
 }
